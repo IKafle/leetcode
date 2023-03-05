87b86ab66682d2f1a16f5bac8f21b728d883c35a
ishwor <echo.ishwor@gmail.com>
2023-03-05 16:37:23 +0545

    leetcode-02: bruteforce solution

--- EASY/02.py
+++ EASY/02.py
@@ -16,7 +16,19 @@ Input: x = 10
 Output: false
 Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
 """
-
+import math
 class Solution:
+    #Bruteforce Solution
     def isPalindrome(self, x: int) -> bool:
-        pass
+        sum : int = 0
+        num : int = x
+        for _ in range(0, len(str(x)),1):
+            digit : int = num % 10
+            num = int(num / 10)            
+            power:int = 0 if num == 0 else len(str(num))
+            sum = sum + digit * (10 ** power)
+
+        return sum == x
+
+if __name__=="__main__":
+    print(Solution().isPalindrome(313))
