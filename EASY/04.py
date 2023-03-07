2a81824cbdbb071e6e6209c51992848bf72c50fa
ishwor <echo.ishwor@gmail.com>
2023-03-07 23:52:26 +0545

    leetcode-04 : optimal solution

--- EASY/04.py
+++ EASY/04.py
@@ -15,8 +15,37 @@ Input: strs = ["dog","racecar","car"]
 Output: ""
 Explanation: There is no common prefix among the input strings.
 
+Python Slicing example : https://railsware.com/blog/python-for-machine-learning-indexing-and-slicing-for-lists-tuples-strings-and-other-sequential-types/
+
 """
-from typing import List
+from typing import List, Dict
 class Solution:
+    """
+    Time complexity: O(N*M) 
+    - Here : N = number of items in the array
+             M = length of the longest item (string) in the array.
+    - Total number of comparisons = (N-1)M
+    - In the worst case scenario, every character of each string is compared with the 
+      corresponding character of the prefix.
+
+    Space complexity : O(1)
+    - Constant amount of extra space is used to store the prefix, the loop variable i, and the loop conditions
+      regardless of the input size. 
+    """
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        pass
+        if not strs:
+            return ''
+        prefix = strs[0]
+        for index in range(1, len(strs) , 1):
+            while strs[index].find(prefix) !=0:
+                prefix = prefix[0:len(prefix)-1]
+
+                if not prefix:
+                    return prefix
+        
+        return prefix
+
+        
+if __name__ == "__main__":
+    strs = ["flower","flow","flight"]
+    print(Solution().longestCommonPrefix(strs))
