706bb0e5edbc0dc37036f73ffbd5881238bf5d14
ishwor <echo.ishwor@gmail.com>
2023-03-08 22:24:35 +0545

    leetcode-05 : valid parentheses using stack DS

--- EASY/05.py
+++ EASY/05.py
@@ -24,7 +24,56 @@ Output: false
 
 """
 
-
+from typing import List
 class Solution:
+    """
+    Data Structure Used : Stack
+    stack = []
+    append() - push item into stack
+    stack[size-1] - Get the most recently pushed item from the stack
+
+    Time complexity: O(n) 
+    -  the loop iterates over the string once, performing constant-time operations 
+        (i.e., pushing, popping, or checking the top of the stack) at each character.
+
+    Space complexity : O(n)
+    - The size of the stack increases with the input size. 
+    """
     def isValid(self, s: str) -> bool:
-        pass
+
+        if not s:
+            return True
+
+        # If the string begins with these characters, we will call it an invalid string
+        invalids : List[str] = [')',']','}']
+        stack : List[str] = []
+
+        # For safety, we will push the first character into the stack
+        stack.append(s[0])
+
+        for i in range(1, len(s), 1):
+            size = len(stack)
+            
+            # Check if we have invalid character once the stack is empty
+            if size == 0 and s[i] in invalids:
+                return False
+            
+            condition1 : bool = s[i] == ')' and stack[size-1] == '('
+            condition2 : bool = s[i] == '}' and stack[size-1] == '{'
+            condition3 : bool = s[i] == ']' and stack[size-1] == '['
+
+            if any([condition1 , condition2 , condition3]):
+                # Remove from stack if the incoming character is any opening parentheses
+                # and its closing parentheses present on the TOS
+                stack.pop() 
+            else:
+                # If the opening parentheses are not mached with TOS, keep pushing into the stack
+                stack.append(s[i])
+        
+        # The string is valid if the stack is empty
+        return len(stack) == 0
+    
+
+if __name__=='__main__':
+    string:str = "(){}}{"
+    print(Solution().isValid(string))
