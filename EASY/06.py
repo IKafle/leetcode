21a93d53d964bb8b4f3d7f835daac251ffca5f6a
ishwor <ishwor.kafle@logicinfo.com>
2023-03-16 23:13:53 +0545

    leetcode-06 : optimal solution

--- EASY/06.py
+++ EASY/06.py
@@ -23,6 +23,7 @@ Output: [0]
 """
 
 from typing import Optional
+import sys
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -32,4 +33,67 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+     
+        # Initialize two pointers to the heads of both the lists.
+        p : Optional[ListNode] = list1 # Pointer to the current node in list1
+        q : Optional[ListNode] = list2 # Pointer to the current node in list2
+
+        # Handle case where one or both input lists are empty
+        if not p: return q # If list1 is None or empty, return list2
+        if not q: return p # If list2 is None or empty, return list1
+
+        # Create a dummy head node for the sorted merged list
+        sorted = ListNode(~sys.maxsize) # Assign a dummy node with a very small -ve value.
+                                        # A bitwise complement of the maximum integer value
+
+        # Initialize a variable to keep track of the head of the sorted merged list
+        new_head : Optional[ListNode] = None # Pointer to the head of the merged list
+
+         # Determine which node should come first in the merged list (Node with smallest value )
+        if p.val <= q.val:
+            sorted = p # Set the dummy node to the first node of list1
+            p = sorted.next # When dummy points to p, move P to the next node in list1
+        else:
+            sorted = q # Set the dummy node to the first node of list2
+            q = sorted.next # When dummy points to q, move q to the next node in list2
+
+         # Set the head of the sorted merged list
+        new_head = sorted # Set the head of the merged list to the first node in the sorted list
+
+        # Merge the rest of the nodes from both input lists
+        while p and q:
+
+            # Compare the values of the current nodes in both input lists
+            if p.val <= q.val:
+                # Add the current node from list1 to the end of the sorted merged list
+                sorted.next = p # Set the next node in the merged list to the current node in list1
+                sorted = p # Move the dummy node to the current node in list1
+                p = sorted.next # Move to the next node in list1
+            else:
+                # Add the current node from list2 to the end of the sorted merged list
+                sorted.next = q # Set the next node in the merged list to the current node in list2
+                sorted = q # Move the dummy node to the current node in list2
+                q = sorted.next # Move to the next node in list2
+
+        # Add any remaining nodes from list1 or list2 to the end of the sorted merged list
+        if p == None : sorted.next = q # If list1 is exhausted, add the rest of list2 to the end of the merged list
+        if q == None : sorted.next = p # If list2 is exhausted, add the rest of list1 to the end of the merged list
+
+        # Return the head of the sorted merged list
+        return new_head
+
+
+
+        
+if __name__ == "__main__":
+
+    list1 = ListNode(1, ListNode(2, ListNode(4)))
+    list2 = ListNode(1, ListNode(3, ListNode(4)))
+
+    s = Solution()
+
+    merged_list = s.mergeTwoLists(list1, list2)
+    while merged_list:
+        print(merged_list.val, end=' ')
+        merged_list = merged_list.next
+        print()        
